5ffab6b4593a81764546e529ffffe9ea570e29e9
Mohammed Raza Khan <rk6265766@gmail.com>
2021-06-02 21:07:20 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from '../src/content/Navigation';
+import Header from '../src/content/Header'
+import Stack from '../src/content/Stack';
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Header />
+      <Stack />
+    </div>
+  );
+}
+
+export default App;
